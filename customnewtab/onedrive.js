be90f3ed8c54f631c2dd56bb9ed25405e3e1ded1
stargateprovider <kana46@windowslive.com>
2019-11-15 12:09:24 +0200

    Do download

--- customnewtab/onedrive.js
+++ customnewtab/onedrive.js
@@ -1,15 +1,43 @@
-const odOptions = {
-  clientId: "5024a142-154d-45c4-9ca4-2013bca8919a",
-  action: "query",
-  multiSelect: true,
-  openInNewWindow: false,
-  advanced: {
-  	redirectUri: "https://stargateprovider.github.io/infoleht/customnewtab/odt.html"
-  },
-  success: function(files) { console.log(files) },
-  cancel: function() { console.log("CANCEL") },
-  error: function(error) { console.log(error) }
+var oneDriveFilePickerError = function() {
+	console.log('OneDrive Launch Failed!');
 }
+var oneDriveFilePickerSuccess = function(files) {
+	console.log(files);
+	var odOptions = {
+		clientId: "5024a142-154d-45c4-9ca4-2013bca8919a",
+		action: "download",
+		multiSelect: true,
+		openInNewWindow: false,
+		advanced: {
+			/*redirectUri: "https://stargateprovider.github.io/infoleht/customnewtab/odt.html",*/
+			queryParameters: "select=id,name,size,file,folder,@microsoft.graph.downloadUrl",
+			filter: "folder,.json",
+			accessToken: files.accessToken
+		},
+		success: 'oneDriveFilePickerSuccess',
+		cancel: 'oneDriveFilePickerCancel',
+		error: 'oneDriveFilePickerError'
+	}
+	OneDrive.open(odOptions);
+}
+var oneDriveFilePickerCancel = function(e) {
+	console.log('OneDrive Launch Cancelled!');
+}
+
 function launchOneDrivePicker(){
-  OneDrive.open(odOptions);
+	var odOptions = {
+		clientId: "5024a142-154d-45c4-9ca4-2013bca8919a",
+		action: "query",
+		multiSelect: true,
+		openInNewWindow: false,
+		advanced: {
+			/*redirectUri: "https://stargateprovider.github.io/infoleht/customnewtab/odt.html",*/
+			queryParameters: "select=id,name,size,file,folder,@microsoft.graph.downloadUrl",
+			filter: "folder,.json"
+		},
+		success: 'oneDriveFilePickerSuccess',
+		cancel: 'oneDriveFilePickerCancel',
+		error: 'oneDriveFilePickerError'
+	}
+	OneDrive.open(odOptions);
 }
