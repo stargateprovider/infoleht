043da0f2565ed67aab0424cf532ce7921560dbc5
stargateprovider <kana46@windowslive.com>
2019-11-15 11:16:54 +0200

    Change order in odt.html

--- customnewtab/onedrive.js
+++ customnewtab/onedrive.js
@@ -2,7 +2,9 @@ const odOptions = {
   clientId: "5024a142-154d-45c4-9ca4-2013bca8919a",
   action: "query",
   multiSelect: true,
-  advanced: {},
+  advanced: {
+  	redirectUri: "https://stargateprovider.github.io/infoleht/customnewtab/odt.html"
+  },
   success: function(files) { console.log(files) },
   cancel: function() { console.log("CANCEL") },
   error: function(error) { console.log(error) }
