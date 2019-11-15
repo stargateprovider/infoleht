19a786a5ef125ebf5bea254d33b7a53cc22ce668
stargateprovider <kana46@windowslive.com>
2019-11-15 11:28:22 +0200

    not in newwindow

--- customnewtab/onedrive.js
+++ customnewtab/onedrive.js
@@ -2,6 +2,7 @@ const odOptions = {
   clientId: "5024a142-154d-45c4-9ca4-2013bca8919a",
   action: "query",
   multiSelect: true,
+  openInNewWindow: false,
   advanced: {
   	redirectUri: "https://stargateprovider.github.io/infoleht/customnewtab/odt.html"
   },
