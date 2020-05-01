5ba0a4bd2cf4162d4c4c7395e380686a791c48ba
stargateprovider <kana46@windowslive.com>
2020-05-02 01:26:14 +0300

    Täiendused viimasele failile+

--- assets/main.js
+++ assets/main.js
@@ -79,24 +79,28 @@ window.onload = function() {
 	// Add youtube, telegram, in5D icons next to their links
 	var icon, links = document.querySelectorAll("li > a");
 	for (var i=0; i<links.length; i++) {
-		if (links[i].href.indexOf("youtu") > -1) {
+		if (links[i].href.match(/[/.]youtu[.b][be][e.]/)) {
 			icon = new Image(); //document.createElement("img");
 			icon.src = "https://s.ytimg.com/yts/img/favicon-vfl8qSV2F.ico";
 			icon.alt = "yt_icon";
-			links[i].insertAdjacentElement("beforebegin", icon);
+		}
+		else if (links[i].href.indexOf(".bitchute.") > -1) {
+			icon = new Image();
+			icon.src = "https://www.bitchute.com/static/v112/images/favicon-32x32.png";
+			icon.alt = "bitchute_icon";
 		}
 		else if (links[i].href.indexOf("telegram") > -1) {
 			icon = new Image();
 			icon.src = "https://www.telegram.ee/wp-content/themes/telegram/favicon/favicon.ico"
 			icon.alt = "telegram_icon";
-			links[i].insertAdjacentElement("beforebegin", icon);
 		}
 		else if (links[i].href.indexOf("in5d") > -1) {
 			icon = new Image();
 			icon.src = "https://in5d.com/wp-content/uploads/2019/03/cropped-favicon5-32x32.jpg"
 			icon.alt = "in5d_icon";
-			links[i].insertAdjacentElement("beforebegin", icon);
 		}
+		else {continue;}
+		links[i].insertAdjacentElement("beforebegin", icon);
 	}
 
 	// Load theme based on cookies
