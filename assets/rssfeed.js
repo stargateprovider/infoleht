978df068caa4326c28aef6ed113d333b376552ac
stargateprovider <kana46@windowslive.com>
2021-02-12 20:02:48 +0200

    Uus CORS proxy jm

--- assets/rssfeed.js
+++ assets/rssfeed.js
@@ -2,22 +2,23 @@ const REGEX = new RegExp("item>\\s*<title>(?<name>.+?)</title>[\\s\\S]*?<link>(?
 	 REGEX2 = new RegExp("entry>[\\s\\S]+?<title>(?<name>.+?)<[\\s\\S]+?href=\"(?<link>.+?)\"[\\s\\S]+?published>(?<date>.+?)<[\\s\\S]+?thumbnail url=\"(?<desc>.+?)\"", "g");
 
 async function fetchFeeds(sources, n) {
+	//Proxyd: https://gist.github.com/jimmywarting/ac1be6ea0297c16c477e17f8fbe51347
 	const parser = new DOMParser(),
-		  proxy = "https://cors-anywhere.herokuapp.com/",
+		  proxy = "https://api.allorigins.win/raw?url=",
+		  promises = [],
 		  dataArray = [];
 
 	for (var i = 0; i < sources.length; i++) {
 		let selectedRegex = sources[i].indexOf("youtube.") == -1 ? REGEX : REGEX2;
 
-		try {
-			await fetch(proxy + sources[i])
+		promises.push(
+			fetch(proxy + sources[i])
 			.then(file => file.text())
-			.then(str => dataArray.push(...Array.from(str.matchAll(selectedRegex))));
-		} catch(err) {
-			console.log("Failed to receive data from " + sources[i]);
-			console.error(err)
-		}
+			.then(str => dataArray.push(...Array.from(str.matchAll(selectedRegex))))
+			.catch(err => console.error(err + " " + sources[i]))
+		);
 	}
+	await Promise.allSettled(promises);
 
 	dataArray.sort((a,b) => {
 		let da = new Date(a.groups.date),
